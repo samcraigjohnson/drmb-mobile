f10b595daacb4ee1b0155429e40deccb85c5f0a1
Sam VB <you@example.com>
2014-04-18 08:21:59 -0400

    init commit

---
+++ ddpclient.py
@@ -0,0 +1,60 @@
+from ws4py.client.threadedclient import WebSocketClient
+import json
+import uuid
+
+
+class DDPClient(WebSocketClient):
+
+	DDP_VER = ["pre1"]
+	WS_URL = "ws://{}:{}/websocket"
+
+	def __init__(self, server, port=80):
+		url = self.WS_URL.format(server, str(port))
+		WebSocketClient.__init__(self, url)
+		self.connected = False
+		self.subs = []
+
+
+	def send(self, msg_dict):
+		message = json.dumps(msg_dict)
+		print "Sending message...", message
+		super(DDPClient, self).send(message)
+
+	#called after handshake confirmed
+	def opened(self):
+		self.send({"msg": "connect", "version": self.DDP_VER[0], "support": self.DDP_VER})
+		print "attempting to handshake"
+
+	def closed(self, code, reason=None):
+		print "Shut down", code, reason
+
+	def received_message(self, m):
+		print m
+		msg = json.loads(str(m))
+		if(msg.get('msg') == "connected"):
+			self.connected = True
+			print msg
+
+	def subscribe(self, sub_name, params=None):
+		msg = {"msg" : "sub"}
+		msg['id'] = "testSub"
+		msg['name'] = sub_name
+		if(params != None):
+			msg['params'] = params
+
+		self.send(msg)
+
+if __name__ == '__main__':
+	try:
+		ws = DDPClient("localhost", 3000)
+		ws.connect()
+		count = 1
+		while True:
+			if ws.connected and count == 1:
+				print "WORKS!"
+				print "subscribing"
+				ws.subscribe("userData")
+				count -= 1
+
+	except KeyboardInterrupt:
+		ws.close()
