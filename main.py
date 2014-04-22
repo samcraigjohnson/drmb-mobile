6ee83a6566f493d99b51f5cc86cf716a02f87af1
Sam VB <you@example.com>
2014-04-21 20:33:11 -0400

    experimenting with kivy

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.layout import Layout
+from kivy.uix.textinput import TextInput
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
 from kivy.clock import Clock
 
-class LoginScreen(Layout):
+class LoginScreen(Widget):
 	f_username = ObjectProperty(None)
 	f_password = ObjectProperty(None)
 
-	def print_names(self, dt):
-		print self.f_username, ":", self.f_password
-	
+	def login(self):
+		uname = self.f_username.text
+		pwd = self.f_password.text
+		print uname, pwd
+
 class MoneybagsApp(App):
 
 	def build(self):
-		screen = LoginScreen()
-		Clock.schedule_interval(screen.print_names, .5)
-		return screen
+		return LoginScreen()
 
 if __name__ == '__main__':
 	MoneybagsApp().run()
